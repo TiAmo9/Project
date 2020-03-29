cd83159dccf48b58d0811505276fb9d33835a8e5
TiAmo9 <kumar88517@gmail.com>
2020-03-30 01:49:50 +0530

    readme

---
+++ BETA.sh
@@ -0,0 +1,17 @@
+";
+sleep 2
+echo "████████████████████"
+echo " Uᴘᴅᴀᴛɪɴɢ ғɪʟᴇs.../Cʜᴇᴄᴋɪɴɢ ᴅᴇᴠɪᴄᴇ "
+echo "████████████████████"
+echo " "
+sleep 5
+echo " Dᴏɴᴇ�� "
+echo "
+"
+echo -e " [CONNECTED TO THE SERVER] "
+echo -e " [NOW RUN PUBG FROM TERMUX] "
+echo -e " [AND WHEN YOU GO IN LOBBY] "
+echo -e " [MINIMIZE PUBG ONCE] "
+echo "
+"
+sleep 2
---
+++ LOBBY.sh
@@ -0,0 +1,30 @@
+";
+echo "██████████████████████████"
+echo "██████████50%"
+echo ""
+sleep 2
+echo "██████████████████████████"
+echo "██████████80%"
+echo ""
+sleep 2
+echo "██████████████████████████"
+echo "██████████100%"
+echo ""
+sleep 2
+echo "Pʟᴇᴀsᴇ Wᴀɪᴛ..."
+echo ""
+echo "Aᴜᴛᴏᴍᴀᴛɪᴄ Sᴇᴛᴜᴘ Fɪʟᴇs..."
+echo ""
+echo "Yᴏᴜ Jᴜsᴛ Gᴏ Bᴀᴄᴋ Iɴ Pᴜʙɢ"
+echo ""
+echo "Dᴏɴᴛ Fᴏʀɢᴇᴛ Tᴏ Usᴇ Mᴇᴍᴍᴏʀʏ Aɴᴛɪʙᴀɴ Lᴜᴀ Iɴ Lᴏʙʙʏ"
+mv /data/data/com.tencent.ig/lib/libtprt.so /storage/emulated/0/Download/RA-ONE_GL/REAL-X
+mv /data/data/com.tencent.ig/lib/libUE4.so /storage/emulated/0/Download/RA-ONE_GL/REAL-X
+mv /data/data/com.tencent.ig/lib/libzlib.so /storage/emulated/0/Download/RA-ONE_GL/REAL-X
+sleep 2
+mv /storage/emulated/0/Download/RA-ONE_GL/FAKE-Y/libzlib.so /data/data/com.tencent.ig/lib/libzlib.so
+mv /storage/emulated/0/Download/RA-ONE_GL/FAKE-Y/libUE4.so /data/data/com.tencent.ig/lib/libUE4.so
+mv /storage/emulated/0/Download/RA-ONE_GL/FAKE-Y/libtprt.so /data/data/com.tencent.ig/lib/libtprt.so
+sleep 2
+am start -n com.tencent.ig/com.epicgames.ue4.SplashActivity
+echo "Dᴏɴᴇ"
